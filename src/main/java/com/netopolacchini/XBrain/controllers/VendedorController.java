b49c355e5a3208de762fe78b8e46959e4ea3c701
Neto Polacchini <64282566+NetoPolacchini@users.noreply.github.com>
2023-04-24 13:36:13 -0300

    Create VendedorController.java

---
+++ src/main/java/com/netopolacchini/XBrain/controllers/VendedorController.java
@@ -0,0 +1,58 @@
+package com.netopolacchini.XBrain.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.netopolacchini.XBrain.models.Vendedor;
+import com.netopolacchini.XBrain.services.VendedorService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/vendedor/")
+@Validated
+public class VendedorController {
+    @Autowired
+    private VendedorService vendedorService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vendedor> findById(@PathVariable Long id) {
+        Vendedor obj = this.vendedorService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping("/{id}")
+    @Validated
+    public ResponseEntity<Void> create (@Valid @RequestBody Vendedor obj){
+        this.vendedorService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated
+    public ResponseEntity<Void> update (@Valid @RequestBody Vendedor obj, @PathVariable Long id){
+        obj.setId(id);
+        this.vendedorService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        this.vendedorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
