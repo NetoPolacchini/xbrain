5e230e1f5c47aab6fd32fa039d02bc27da49483c
Neto Polacchini <64282566+NetoPolacchini@users.noreply.github.com>
2023-04-24 11:25:05 -0300

    initial commit

---
+++ src/main/java/com/netopolacchini/XBrain/XBrainApplication.java
@@ -0,0 +1,13 @@
+package com.netopolacchini.XBrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netopolacchini/XBrain/XBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netopolacchini.XBrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
