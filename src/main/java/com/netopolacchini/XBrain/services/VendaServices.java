c4531dcd449ff02cab52b2f4c3754d8faa1cc8e7
Neto Polacchini <64282566+NetoPolacchini@users.noreply.github.com>
2023-04-24 13:51:49 -0300

    Create VendaServices.java

---
+++ src/main/java/com/netopolacchini/XBrain/services/VendaServices.java
@@ -0,0 +1,66 @@
+package com.netopolacchini.XBrain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.netopolacchini.XBrain.models.Venda;
+import com.netopolacchini.XBrain.models.Vendedor;
+import com.netopolacchini.XBrain.repositories.VendaRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class VendaServices {
+
+    @Autowired
+    private VendaRepository vendaRepository;
+
+    @Autowired
+    private VendedorService vendedorService;
+
+    public Venda findById (Long id){
+        Optional<Venda> vendedor = this.vendaRepository.findById(id);
+        return vendedor.orElseThrow( () -> new RuntimeException(
+            "Venda não localizada. ID: "+id
+        ));
+    }
+
+    public List<Venda> findAllByVendedorId (Long id){
+        List<Venda> vendas = this.vendaRepository.findByVendedor_Id(id);
+        return vendas;
+    }
+
+    @Transactional
+    public Venda create(Venda obj){
+        Vendedor vendedor = this.vendedorService.findById(obj.getVendedor().getId());
+        obj.setId(null);
+        obj.setVendedor(vendedor);
+
+        obj = this.vendaRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Venda update (Venda obj){
+        Venda newObj = findById(obj.getId());
+        newObj.setDate(obj.getDate());
+        newObj.setPrice(obj.getPrice());
+        return this.vendaRepository.save(newObj);
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        findById(id);
+        try {
+            this.vendaRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                "Não foi possível deletar a venda. ID: "+id
+            );
+        }
+    }
+    
+}
